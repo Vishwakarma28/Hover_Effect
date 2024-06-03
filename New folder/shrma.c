695b5b8788f07a9e4538c8b2bfce05990b8e8121
Vishwakarma28 <Amit9301736578@gmail.com>
2024-06-03 19:22:29 +0530

    first commit

---
+++ New folder/shrma.c
@@ -0,0 +1,124 @@
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("enter a int");
+//     scanf("%d",&n);
+//     return 0 ;
+// }
+// WAP to display the seris `1 to n is a user number print in sum of all even number and odd number
+// #include<stdio.h>
+// int main(){
+// int i=1,n,sum1=0,sum2=0;
+// printf("Enter a int");
+// scanf("%d",&n);
+// for(;n<=1;i++){
+//     printf("%d \n",i);
+//     if(n%2==0){
+//         sum1=sum1+i;
+       
+//     }
+//     else{
+//         sum2=sum2+i;
+//     }
+   
+// }
+//  printf("even sum is %d",sum1);
+//      printf("odd sum is %d",sum2);
+//      return 0;
+// }
+// WAP to use a do while loop to ask the user to enter the two number and print it their sum repeat the process if the user wishes
+//enter no of month =15
+//invalid month pls try again
+//enter no of month =-15
+//invalid month pls try again
+//enter no of month =5
+//thank u visit again
+// #include<stdio.h>
+// int main(){
+//     int a,b,sum=0;
+//     char choise;
+//     do{
+//     printf("enter a number");
+//     scanf("%d%d",&a&b);
+//     sum=a+b;
+//     printf("this sum is=%d",sum);
+//     printf("\n do u want continue(y/n)=");
+//     scanf("%c",choise
+//     )
+//     }
+//     while(choise==y)
+    
+        
+//     sum=a+b;
+   
+
+    
+//     return 0;
+// }
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     do{
+//     printf("enter a month");
+//     scanf("%d",&n);
+//    if(n<1||n>12)
+//    printf("invalid month try again");
+//     }
+//     while(n<1||n>12);
+//     printf("valid month");
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main(){
+//     int d,m,y;
+//     do{
+//     printf("enter a DOB");
+//     scanf("%d%d%d",&);
+//    if(n<1||n>12)
+//    printf("invalid month try again");
+//     }
+//     while(n<1||n>12);
+//     printf("valid month");
+//     return 0;
+// }
+// WAP to accept integer from user and print a odd number and display a odd series that number user print to 1
+//  #include<stdio.h>
+//  int main(){
+//     int n;
+//     printf("enter a integer");
+//     scanf("%d",&n);
+//           if(n%2==0)
+//           n--;
+//         while(n>=1){
+//         printf("\n %d",n);
+//         n=n-2;
+//     }
+//   return 0;
+//  }
+// WAP to dispaly series 1 to n, n is given  number and print of all sum even number and odd number
+// #include<stdio.h>
+// int main(){
+//     int n,i,sum1=0,sum2=0;
+//     printf("Enter a number:");
+//     scanf("%d",&n);
+//     for(i=1;i<=n;i++)
+//     {
+//         // printf("%d \n",i);
+//         if(i%2==0){
+//             sum1=sum1+i;
+//         }
+//         else{
+//             sum2=sum2+i;
+//         }
+
+//     }
+//       printf("\n sum of even %d",sum1);
+//       printf("\n sum of odd %d",sum2);
+//     return 0;
+// }
+#include<stdio.h>
+int main(){
+    int 
+    return 0;
+}
---
+++ New folder/vertesOfForLoop.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int i,j;
+    for(i=1,j=10;i<=10||j>=1;i++,j--){
+        printf("\n i=%d,j=%d",i,j);
+    }
+
+    return 0;
+}
